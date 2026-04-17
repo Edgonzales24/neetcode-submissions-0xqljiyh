e6fbc8d993a22bb29070518052d3191cc2de65d3
MaverickEFG <57543088+Edgonzales24@users.noreply.github.com>
2026-04-17 12:40:27 -0400

    Add: design-word-search-data-structure - submission-13

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-13.py
@@ -0,0 +1,36 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def backtrack(start, node):
+            curr = node
+            for i in range(start, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in curr.children.values():
+                        if backtrack(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.endOfWord
+        
+        return backtrack(0, self.root)
