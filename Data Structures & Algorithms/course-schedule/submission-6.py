5ce17bf7ccc6128abdcfac6f59659dac5c6172eb
MaverickEFG <57543088+Edgonzales24@users.noreply.github.com>
2026-04-27 00:29:04 -0400

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {i : [] for i in range(numCourses)}
+        for pre, crs in prerequisites:
+            adj[crs].append(pre)
+        visited, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visited:
+                return True
+            
+            cycle.add(crs)
+            for pre in adj[crs]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(crs)
+            visited.add(crs)
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
