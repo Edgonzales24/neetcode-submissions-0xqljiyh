90b95198b522c88ce64832c63fe1cb09ea86096e
Ed Gonzales <57543088+edfgonzales@users.noreply.github.com>
2026-08-18 19:18:14 -0400

    Add: pacific-atlantic-water-flow - submission-12

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-12.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pacific = [[False] * COLS for i in range(ROWS)]
+        atlantic = [[False] * COLS for i in range(ROWS)]
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def bfs(source, ocean):
+            q = deque(source)
+            while q:
+                r, c = q.popleft()
+                ocean[r][c] = True
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < ROWS and 0 <= nc < COLS and
+                        heights[nr][nc] >= heights[r][c] and
+                        not ocean[nr][nc]):
+                        q.append((nr, nc))
+        
+        pac, atl = [], []
+        for r in range(ROWS):
+            pac.append((r, 0))
+            atl.append((r, COLS - 1))
+        
+        for c in range(COLS):
+            pac.append((0, c))
+            atl.append((ROWS - 1, c))
+        
+        bfs(pac, pacific)
+        bfs(atl, atlantic)
+        
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if pacific[r][c] and atlantic[r][c]:
+                    res.append((r, c))
+        return res
+
+
